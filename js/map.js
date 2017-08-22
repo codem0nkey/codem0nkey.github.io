87bcaed4755c3540153eaa492d18c94ce1ef482f
DAVIS <md1691@att.com>
2017-08-21 22:22:06 -0700

    JSON Data for Bars and List displaying

--- js/map.js
+++ js/map.js
@@ -1,28 +1,99 @@
-// map variable
 var map;
 
-// initialize the map
+// Create a new blank array for all the listing markers.
+var markers = [];
+
 function initMap() {
-	// constructor to create a new map JS object.
-	map = new google.maps.Map(document.getElementById('map'),{
-		center: {lat: 33.7997535, lng: -118.1231491},
-		zoom: 15
-	});
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 33.7997535, 
+			lng: -118.1231491
+        },
+        zoom: 15
+    });
 
-	//Create a marker on map
-	var Lowes = {lat: 33.807822, lng: -118.121712};
-	var marker = new google.maps.Marker({
-		position: Lowes,
-		map: map,
-		title: 'First Marker!'
-	});
+	// Bar Locations
+    var locations = [{
+            title: "Tracy's Bar & Grill",
+            location: {
+                lat: 33.810862,
+                lng: -118.123903
+            }
+        },
+        {
+            title: "Poor Richard's Cocktails",
+            location: {
+                lat: 33.794987,
+                lng: -118.107528
+            }
+        },
+        {
+            title: "Interlude",
+            location: {
+                lat: 33.788887,
+                lng: -118.133500
+            }
+        },
+        {
+            title: "Annex",
+            location: {
+                lat: 33.795937,
+                lng: -118.142036
+            }
+        },
+        {
+            title: "Blondie's",
+            location: {
+                lat: 33.796683,
+                lng: -118.143140
+            }
+        },
+    ];
 
-	// info window for marker
-	var infowindow = new google.maps.InfoWindow({
-		content: 'Welcome to Lowes home improvement center for suckas!!'
-	});
-	// click for info window
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
-	});
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
+		
+		$("#list").append("<li>" + title + "</li>");
+		
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.setMarker = null;
+        });
+    }
 }
