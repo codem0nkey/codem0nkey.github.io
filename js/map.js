4529b31f0f0d7d5ab37d7dcbfbf38bf0b60a7c56
DAVIS <md1691@att.com>
2017-11-11 14:42:59 -0800

    Working Map and Filter

--- js/map.js
+++ js/map.js
@@ -1,108 +1,127 @@
-var map;
+// Bar Locations
+var locationData = [{
+		'name': "Tracy's Bar & Grill",
+		'latLng': {lat:33.810862, lng: -118.123903}
+	},
+	{
+		'name': "Poor Richard's Cocktails",
+		'latLng': {lat:33.794987, lng:-118.107528}
+	},
+	{
+		'name': "Interlude",
+		'latLng': {lat:33.788887, lng:-118.133500}
+	},
+	{
+		'name': "Annex",
+		'latLng': {lat:33.795937, lng:-118.142036}
+	},
+	{
+		'name': "Blondie's",
+		'latLng': {lat:33.796683, lng:-118.143140}
+	}
+];
 
-// Create a new blank array for all the listing markers.
-var markers = [];
+var koViewModel = function() {
+  "use strict";
+  var self = this;
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: 33.7997535, 
-			lng: -118.1231491
-        },
-        zoom: 15
+  var bounds = new google.maps.LatLngBounds();
+
+  // Construct the Google Map.
+  self.googleMap = new google.maps.Map(document.getElementById('map'), {
+  });
+
+  // Build "Place" objects from locationData object.
+  self.allPlaces = [];
+  locationData.forEach(function(place) {
+    self.allPlaces.push(new Place(place));
+  });
+
+  // Place constructor function.
+  function Place(dataObject) {
+    this.name = dataObject.name;
+    this.latLng = dataObject.latLng;
+  }
+  
+
+  // Set infowindow
+  var infowindow = new google.maps.InfoWindow({
+    content: "bar"
+  });
+
+  
+  // Build Markers via the Maps API and place them on the map.
+  self.allPlaces.forEach(function(place) {
+    var markerOptions = {
+      map: self.googleMap,
+      position: place.latLng
+    };
+    
+    place.marker = new google.maps.Marker(markerOptions);
+    bounds.extend(place.marker.position);
+
+    google.maps.event.addListener(place.marker, 'click', function() {
+      place.marker.setAnimation(google.maps.Animation.BOUNCE);
+      window.setTimeout(function() {
+        place.marker.setAnimation(null);
+      }, 1500);
+	  infowindow.setContent(place.name);
+      infowindow.open(self.googleMap, place.marker);	
     });
 
-	// Bar Locations
-    var locations = [{
-            title: "Tracy's Bar & Grill",
-            location: {
-                lat: 33.810862,
-                lng: -118.123903
-            },
-			visible: false
-        },
-        {
-            title: "Poor Richard's Cocktails",
-            location: {
-                lat: 33.794987,
-                lng: -118.107528
-            }, 
-			visible: true
-        },
-        {
-            title: "Interlude",
-            location: {
-                lat: 33.788887,
-                lng: -118.133500
-            },
-			visible: true
-        },
-        {
-            title: "Annex",
-            location: {
-                lat: 33.795937,
-                lng: -118.142036
-            },
-			visible: true
-        },
-        {
-            title: "Blondie's",
-            location: {
-                lat: 33.796683,
-                lng: -118.143140
-            },
-			visible: true
-        },
-    ];
 
+    google.maps.event.addListener(infowindow, 'closeclick', function(){
+    });
+
+    //This function is triggered when a list item is clicked.
+    self.openInfoWindow = function() {
+      google.maps.event.trigger(this.marker, 'click');
+    };
+  });
+
+  self.googleMap.fitBounds(bounds);
+
+  // This array contains places that should show as markers on the map (based on user input).
+  self.visiblePlaces = ko.observableArray();
+  
+  
+  // Initialize map with all places.
+  self.allPlaces.forEach(function(place) {
+    self.visiblePlaces.push(place);
 	
-
-    var largeInfowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-
-    // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < locations.length; i++) {
-		// Get the position from the location array.
-		var position = locations[i].location;
-		var title = locations[i].title;
-		// Create a marker per location, and put into markers array.
-		var marker = new google.maps.Marker({
-			map: map,
-			position: position,
-			title: title,
-			animation: google.maps.Animation.DROP,
-			id: i
-		});
-		// Push the marker to our array of markers.
-		markers.push(marker);
-		// Create an onclick event to open an infowindow at each marker.
-		marker.addListener('click', function() {
-			populateInfoWindow(this, largeInfowindow);
-		});
-		bounds.extend(markers[i].position);
+  });
+  
+  
+  // This observable tracks user input in the search field.
+  self.userInput = ko.observable('');
+  
+  
+  // This filter compares the userInput observable to the visisblePlaces array.
+  // Non-matching places are filtered off of the map.
+  self.filterMarkers = function() {
+    var searchInput = self.userInput().toLowerCase();
+    
+    self.visiblePlaces.removeAll();
+    
+    // This looks at the name of each place and then determines if the user
+    // input can be found within the place name.
+    self.allPlaces.forEach(function(place) {
+      place.marker.setVisible(false);
+      
+      if (place.name.toLowerCase().indexOf(searchInput) !== -1) {
+        self.visiblePlaces.push(place);
 		
-		// put locations in the list
-		$("#list").append("<li>" + title + "</li>");
-	}
-    // Extend the boundaries of the map for each marker
-    map.fitBounds(bounds);
-}
+      }
+    });
+    
+    self.visiblePlaces().forEach(function(place) {
+      place.marker.setVisible(true);
+    });
+  };
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            infowindow.setMarker = null;
-        });
-    }
-}
 
-var weatherApi = 'https://api.apixu.com/v1/current.json?key=d4d70ce13e1740688cb32830172308&q=Long+Beach,+CA';
+};
+
+function initMap() {
+  ko.applyBindings(new koViewModel());
+}
